e2dda74d1cb1440e6b56f6e965a645fb346e0aa0
hieu1966hn <hieu1966hn@gmail.com>
2021-09-05 16:22:53 +0700

    done Form submit SS10

---
+++ SS10/SS10-lythuyet.md
@@ -0,0 +1,22 @@
+Giới thiệu về localStorage là gì?
+
+- localStorage dùng để lưu trữ dữ liệu tại trình duyệt của user, tính năng hữu ích trong HTML 5 dùng để lưu trữ nhiều dạng dữ liệu mà không ảnh hưởng tới hiệu năng website
+  - local Storage: Lưu trữ dữ liệu mà không có thời hạn (kể cả khi trình duyệt bị đóng).
+  - Session Storage: Lưu trữ dữ liệu trong 1 session và sẽ mất khi tab hiện tại bị đóng.
+
+* local Storage: Cách sử dụng:
+  Bước 1: Lưu trữ dữ liệu tại local Storage: (chỉ có thể lưu trữ chuỗi bên trong localStorage).
+
+  - localStorage.setItem('key',"value");
+  - localStorage.key = "value"
+  - localStorage['key'] = "value"
+
+  ==> lưu trữ 1 biến x bất kỳ, ta sử dụng câu lệnh:
+  localStorage.setItem('key',JSON.stringify(x));
+
+  Bước 2: Lấy ra giá trị đã lưu trữ, ta sử dụng phương thức getItem();
+  Cú pháp:
+  let variable = localStorage.getItem("key");
+
+  Bước 3; Xóa dữ liệu đã lưu trữ trên localStorage
+  localStorage.removeItem('key')
---
+++ SS10/app.js
@@ -0,0 +1,89 @@
+// function myFunction (){
+//   document.getElementById("demo").innerHTML = 
+//   `
+//     Noi dung da duoc thay doi
+//   `
+// }
+
+
+
+function myFunction() {
+  let x = document.getElementById("myBtn").
+    attributes[0].name;
+  // luu tru thuoc tinh dau tien cua the co #myBtn
+  document.getElementById("demo").innerHTML = x;
+
+}
+
+
+//  ham doi mau
+// function changeColor(){
+//   // document.getElementById("h1").style.color = "red"
+//   document.querySelector("#h1").style.color = "red"
+// }
+
+//  tao ra 1 bien co ten la h1 de luu tru the minh muon trong do
+const h1 = document.getElementById("tieuDe");
+console.log(h1);
+
+h1.addEventListener("click", function () {
+  console.log("addEventListener hoat dong nha");
+  h1.style.color = "blue"
+})
+
+
+let HumanInfor = []
+
+
+//  lay  ra the form co id tuong ung
+const formSubmit = document.getElementById("info");
+formSubmit.addEventListener("submit", function (event) {
+  event.preventDefault();
+  // ngan su kien load lai trang mac dinh cua trinh duyet
+  console.log("ban da bam vao submit roi day");
+
+  if (formSubmit.name.value.trim() === "") {
+    alert("please type your name");
+  }
+  else if (formSubmit.email.value.trim() === "") {
+    alert("please type your email");
+  }
+  else if (formSubmit.website.value.trim() === "") {
+    alert("please type your website");
+  }
+  else if (formSubmit.comment.value.trim() === "") {
+    alert("please type your comment");
+  }
+  else {
+
+    // khai bao bien data chua toan bo thong tin minh nhap trong form
+    const data = {
+      name: formSubmit.name.value.trim(),
+      email: formSubmit.email.value.trim(),
+      website: formSubmit.website.value.trim(),
+      comment: formSubmit.comment.value.trim()
+    }
+    console.log(data);
+    HumanInfor.push(data);
+  }
+
+  formSubmit.name.value = "";
+  formSubmit.email.value = "";
+  formSubmit.website.value = "";
+  formSubmit.comment.value = "";
+
+})
+
+
+
+// let y = 12345678910;
+// localStorage.setItem("y", JSON.stringify(y));
+// key là y, value:12345678910
+
+// console.log(
+//   localStorage.getItem("z") // khong ton tai bien z
+// );
+
+
+
+
